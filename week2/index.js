51e31949037c18a4503c3a99414877c0b24a780a
Durgesh Nishad <dnishad042@gmail.com>
2023-08-08 14:34:49 +0530

    week2/2.1 finish

--- week2/index.js
+++ week2/index.js
@@ -11,19 +11,28 @@ const port = 3000
 
 function calculateSum(counter){
     var sum=0;
-    for(var i=0;i<counter;i++)
+    for(var i=1;i<=counter;i++)
         sum+=i;
     return sum;
 }
 
 function handleFirst(req, res){
-    var sum= calculateSum(100)
+    var count= req.query.counter;
+    var sum= calculateSum(count)
     res.send('The sum is '+sum)
 }
 app.get('/handlesum', handleFirst)
 
 app.get('/',(req, res)=>{
     res.send('Welcome')
+})
+
+app.post('/createuser',(req, res)=>{
+    res.send('SIGN UP')
+})
+
+app.get('/:username',(req, res) =>{
+    res.send('HEHE')
 })
 
 function done(){
